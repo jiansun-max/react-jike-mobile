e820dcce9a4f2153999de4a93c55fa27b1c61ec5
lin479909332 <lin479909332@gmail.com>
2023-12-17 16:19:08 +0800

    feat(HomeList/index.tsx): 实现HomeList的滚动无限加载

--- src/pages/Home/HomeList/index.tsx
+++ src/pages/Home/HomeList/index.tsx
@@ -1,4 +1,4 @@
-import { Image, List } from 'antd-mobile'
+import { Image, List, InfiniteScroll } from 'antd-mobile'
 // mock数据
 import { useEffect, useState } from 'react'
 import { fetchListAPI } from '@/apis/list'
@@ -13,6 +13,7 @@ const HomeList = (props: Props) => {
     results: [],
     pre_timestamp: '' + new Date().getTime(),
   })
+  // 请求文章列表
   useEffect(() => {
     const getList = async () => {
       try {
@@ -31,6 +32,29 @@ const HomeList = (props: Props) => {
     }
     getList()
   }, [channelId])
+  // 是否有更多数据可以加载
+  const [hasMore, setHasMore] = useState(true)
+  // 加载更多数据的方法
+  const loadMore = async () => {
+    try {
+      const res = await fetchListAPI({
+        channel_id: channelId,
+        timestamp: listRes.pre_timestamp,
+      })
+      const { results, pre_timestamp } = res.data.data
+      // 拼接新数据，存储下一次请求的时间戳
+      setListRes({
+        results: [...listRes.results, ...results],
+        pre_timestamp,
+      })
+      // 没有更多数据时
+      if (results.length === 0) {
+        setHasMore(false)
+      }
+    } catch (error) {
+      throw new Error('请求更多文章列表失败')
+    }
+  }
   return (
     <>
       <List>
@@ -52,6 +76,7 @@ const HomeList = (props: Props) => {
           </List.Item>
         ))}
       </List>
+      <InfiniteScroll loadMore={loadMore} hasMore={hasMore} threshold={10} />
     </>
   )
 }
