f61ca82e2b66737461bfe057c30f7beb4e39ef4d
lin479909332 <lin479909332@gmail.com>
2023-12-17 15:27:13 +0800

    feat(Pages/Home): 发请获取频道列表，并进行页面渲染

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react'
+import { Tabs } from 'antd-mobile'
+import './style.css'
+import { fetchChannelAPI } from '@/apis/list'
+import type { ChannelItem } from '@/apis/list'
+
 const Home = () => {
-  return <div>Home</div>
+  const [channels, setChannels] = useState<ChannelItem[]>([])
+  useEffect(() => {
+    const getChannels = async () => {
+      try {
+        const res = await fetchChannelAPI()
+        if (res.data.message === 'OK') {
+          setChannels(res.data.data.channels)
+        }
+      } catch (error) {
+        throw new Error('获取频道列表失败')
+      }
+    }
+    getChannels()
+  }, [])
+  return (
+    <div>
+      <div className="tabContainer">
+        <Tabs>
+          {channels.map((item) => (
+            <Tabs.Tab title={item.name} key={item.id}>
+              {item.name}
+            </Tabs.Tab>
+          ))}
+        </Tabs>
+      </div>
+    </div>
+  )
 }
 
 export default Home
