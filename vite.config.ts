2e1f60c8f528679efe338c8696ce3716ba3abad4
lin479909332 <lin479909332@gmail.com>
2023-12-17 14:45:16 +0800

    feat(vite.config.ts): 配置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,10 @@ export default defineConfig({
   server: {
     // 设置为true时，运行npm run dev时将自动打开浏览器
     open: true, 
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
